12832574d24b74bb033dac8dd3aa92da23bee91b
Takumi Watanabe <m0119323fd@edu.teu.ac.jp>
2021-06-11 17:48:07 +0900

    修正版実装

---
+++ Assets/WATAMI/Animation/Wait.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wait : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+        anim.SetTrigger("To_Wait");
+    }
+}
